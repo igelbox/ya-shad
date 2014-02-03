e5e2bf0c3f1b0414e67bf4da0314629f0a5a35c0
Vakhurin Sergey <igelbox@gmail.com>
2014-02-03 21:40:17 +0400

    2012h2t1: increase test data size (for slowdown computations)

--- src/shad/pardis/k-means/gen.cpp
+++ src/shad/pardis/k-means/gen.cpp
@@ -2,19 +2,29 @@
 #include <cstdio>
 #include <cmath>
 
-double drand() {
-    return (double) rand() * 100.0 / RAND_MAX;
+static const double//
+        SIZE_X = 100.0,
+        SIZE_Y = 100.0,
+        RADIUS_MAX = 30.0;
+static const unsigned//
+        CLUSTER_COUNT = 50,
+        POINTS_PER_CLUSTER_MIN = 600,
+        POINTS_PER_CLUSTER_MAX = 2400;
+
+template<class T> T uniform(T range) {
+    return range * rand() / RAND_MAX;
 }
 
 int main() {
     srand(1);
-    for (unsigned i = 0; i < 5; i++) {
-        double x = 100.0 * rand() / RAND_MAX;
-        double y = 100.0 * rand() / RAND_MAX;
-        double r = 30.0 * rand() / RAND_MAX;
-        for (unsigned _ = 0; _ < 20; _++) {
+    for (unsigned i = 0; i < CLUSTER_COUNT; i++) {
+        double x = uniform(SIZE_X);
+        double y = uniform(SIZE_Y);
+        double r = uniform(RADIUS_MAX);
+        unsigned cnt = uniform(POINTS_PER_CLUSTER_MAX - POINTS_PER_CLUSTER_MIN) + POINTS_PER_CLUSTER_MIN;
+        for (unsigned _ = 0; _ < cnt; _++) {
             double rr = r * rand() / RAND_MAX;
-            double aa = 2.0 * M_PI * rand() / RAND_MAX;
+            double aa = uniform(2.0 * M_PI);
             printf("%f;%f\n", x + rr * cos(aa), y + rr * sin(aa));
         }
     }
